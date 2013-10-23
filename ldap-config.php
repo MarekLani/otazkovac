16e9b1481e2f39d7a4350c051c56cb4eda31e9a8
lani <lani@ineed.sk>
2013-10-23 14:54:26 +0200

    Ready for github - 1. commit

--- ldap-config.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['ldap'] = array(
-	'host'	 => 'ldaps://ldap.stuba.sk',
-	'port'	 => '636',
-	'basedn' => 'ou=People,dc=stuba,dc=sk'
-);
-	
-?>
